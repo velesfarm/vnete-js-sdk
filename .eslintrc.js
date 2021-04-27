c8a52f2e7339cb38b7396fc178bc16f8a696b4b2
vnete <veles.farmer@gmail.com>
2021-04-27 16:01:49 +0300

    vnete

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+    plugins: [
+        "matrix-org",
+    ],
+    extends: ["plugin:matrix-org/javascript"],
+    env: {
+        browser: true,
+        node: true,
+    },
+    rules: {
+        "no-var": ["warn"],
+        "prefer-rest-params": ["warn"],
+        "prefer-spread": ["warn"],
+        "one-var": ["warn"],
+        "padded-blocks": ["warn"],
+        "no-extend-native": ["warn"],
+        "camelcase": ["warn"],
+        "no-multi-spaces": ["error", { "ignoreEOLComments": true }],
+        "space-before-function-paren": ["error", {
+            "anonymous": "never",
+            "named": "never",
+            "asyncArrow": "always",
+        }],
+        "arrow-parens": "off",
+        "prefer-promise-reject-errors": "off",
+        "quotes": "off",
+        "indent": "off",
+        "no-constant-condition": "off",
+        "no-async-promise-executor": "off",
+        // We use a `logger` intermediary module
+        "no-console": "error",
+    },
+    overrides: [{
+        "files": ["src/**/*.ts"],
+        "extends": ["plugin:matrix-org/typescript"],
+        "rules": {
+            // We're okay being explicit at the moment
+            "@typescript-eslint/no-empty-interface": "off",
+            // While we're converting to ts we make heavy use of this
+            "@typescript-eslint/no-explicit-any": "off",
+            "quotes": "off",
+        },
+    }],
+};
---
+++ README.md
@@ -0,0 +1,4 @@
+# Vnete Javascript SDK
+
+This is the Vnete Client-Server r0 SDK for
+JavaScript. This SDK can be run in a browser or in Node.js.
